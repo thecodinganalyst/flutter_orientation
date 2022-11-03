02ac02675e60dac16e479c5079b68145104f364d
Dennis Cai <dennis.cai@zuhlke.com>
2022-11-03 17:31:24 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Orientation Demo';
+    
+    return MaterialApp(
+      title: appTitle,
+      home: OrientationList(title: appTitle),
+    );
+  }
+}
+
+class OrientationList extends StatelessWidget {
+  final String title;
+
+  const OrientationList({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.count(
+            crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+            children: List.generate(100, (index) {
+              return Center(
+                child: Text(
+                  '$index',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+              );
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
+
